88f4081043044f764f7cdcbb0763b84b2b0f9a4a
Sandrico Provo <Sprovo@outlook.com>
2022-05-28 19:44:45 -0300

    T-116[feat]: Adds section schema & relevant relationships.

--- keystone.ts
+++ keystone.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import { config } from '@keystone-6/core';
 
+import { lists } from './keystoneList';
+
 export default config({
     db: {
         provider: 'postgresql',
         url: process.env.DB_CONNECTION_STRING,
     },
-    lists: {},
+    lists,
 });
