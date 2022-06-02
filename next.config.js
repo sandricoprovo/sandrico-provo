f8a2856f1142046fd920109a6196f046183e77e2
Sandrico Provo <Sprovo@outlook.com>
2022-06-01 21:17:44 -0300

    T-120[chore]: Adds script that starts keystone & next together.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const { withKeystone } = require('@keystone-6/core/next');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withKeystone({
     reactStrictMode: true,
-};
+});
 
 module.exports = nextConfig;
