d2735f7bbaaab4a22fde0f606bec29b3c2b9776a
Sandrico Provo <Sprovo@outlook.com>
2022-06-04 09:21:42 -0300

    T-120[chore]: Adds styled-components to next compiler config.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const { withKeystone } = require('@keystone-6/core/next');
 /** @type {import('next').NextConfig} */
 const nextConfig = withKeystone({
     reactStrictMode: true,
+    compiler: {
+        // Enables the styled-components SWC transform
+        styledComponents: true,
+    },
 });
 
 module.exports = nextConfig;
