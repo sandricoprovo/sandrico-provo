f7db02adce0ce63eb6af5024b329556ee58cbe55
Sandrico Provo <Sprovo@outlook.com>
2022-09-21 20:28:48 -0300

    [feat] Adds Head component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect } from 'react';
 
+import { Head } from '../src/components/Head';
+
 function Home() {
     useEffect(() => {
         document.querySelector('body')?.classList.add('theme-default');
     }, []);
 
-    return <h1 className="header-1">Header</h1>;
+    return (
+        <>
+            <Head title="Sandrico Provo - Software Developer" />
+            <h1 className="header-1">Header</h1>
+        </>
+    );
 }
 
 export default Home;
