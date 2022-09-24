a6ad02c6f07ec80e60abdc9c27c0753231889815
Sandrico Provo <Sprovo@outlook.com>
2022-09-24 08:34:38 -0300

    [fix] Fixes TS bug where default icon props missed mandatory prop.

--- src/components/Shared/Icons/index.ts
+++ src/components/Shared/Icons/index.ts
@@ -2,6 +2,6 @@ import { HOCIcon, IconProps } from './Icons';
 import { Twitter, GitHub, Linkedin } from 'react-feather';
 
 // Exported Icons
-export const TwitterIcon = (props: IconProps = {}) => HOCIcon(Twitter, props);
-export const GitHubIcon = (props: IconProps = {}) => HOCIcon(GitHub, props);
-export const LinkedinIcon = (props: IconProps = {}) => HOCIcon(Linkedin, props);
+export const TwitterIcon = (props: IconProps) => HOCIcon(Twitter, props);
+export const GitHubIcon = (props: IconProps) => HOCIcon(GitHub, props);
+export const LinkedinIcon = (props: IconProps) => HOCIcon(Linkedin, props);
