e97aeb2e1046f21cc56c3e485ae0f6a16b8bdc1e
Sandrico Provo <Sprovo@outlook.com>
2022-09-25 09:55:48 -0300

    [feat] Adds useInView hook to detect element intersections.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { MutableRefObject, useRef } from 'react';
 import styled from 'styled-components';
 import Image from 'next/image';
 import setup1 from '../public/images/setup-1.jpeg';
+import { useInView } from '../src/hooks/useInView';
 
 import { Head } from '../src/components/Head/Head';
 import { Hero } from '../src/components/Home/Hero';
@@ -11,15 +12,51 @@ const ImageContainer = styled.section`
     width: 100%;
     height: 300px;
     margin: 120px 0;
+
+    // TODO: convert this to a motion component to animate exits
+    & > div {
+        width: 100%;
+        height: 100%;
+        transform: translateX(-100vw);
+
+        animation-name: slideImageOver;
+        animation-duration: 1s;
+        animation-timing-function: cubic-bezier(0.46, 0.03, 0.52, 0.96);
+        animation-fill-mode: forwards;
+        animation-delay: 0.4s;
+
+        @keyframes slideImageOver {
+            from {
+                transform: translateX(-100vw);
+            }
+            to {
+                transform: translateX(0);
+            }
+        }
+    }
 `;
 
 function Home() {
+    const imageRef: MutableRefObject<any> = useRef<HTMLDivElement>();
+    const isInView: boolean = useInView<HTMLDivElement | any>(
+        imageRef,
+        '-200px'
+    );
+
     return (
         <>
             <Head title="Sandrico Provo - Software Developer" />
             <Hero />
-            <ImageContainer>
-                <Image src={setup1.src} layout="fill" objectFit="cover" />
+            <ImageContainer ref={imageRef}>
+                {isInView && (
+                    <div>
+                        <Image
+                            src={setup1.src}
+                            layout="fill"
+                            objectFit="cover"
+                        />
+                    </div>
+                )}
             </ImageContainer>
         </>
     );
