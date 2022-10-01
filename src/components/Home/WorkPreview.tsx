8fd1f5fca506e72e8f1aee576da10adba7b2dc79
Sandrico Provo <Sprovo@outlook.com>
2022-10-01 20:31:40 -0300

    [fix] Adds key to ProjectSmall array.

--- src/components/Home/WorkPreview.tsx
+++ src/components/Home/WorkPreview.tsx
@@ -56,6 +56,7 @@ export function WorkPreview() {
 
                     return (
                         <ProjectSmall
+                            key={project.name}
                             image={project.photo}
                             name={project.name}
                         />
