1ca41c2c5e7203c27d7a17405a5d119ccebb2fae
Sandrico Provo <Sprovo@outlook.com>
2022-10-07 23:11:25 -0300

    [feat] Adds fade in animation to Hero Header text.

--- src/components/Hero/HeroHeader.tsx
+++ src/components/Hero/HeroHeader.tsx
@@ -10,6 +10,21 @@ const Header = styled.h1`
     line-height: 100%;
     letter-spacing: 2px;
     text-align: center;
+    opacity: 0;
+    transform: translateY(40px);
+
+    animation: fadeTextIn 0.4s 0.4s cubic-bezier(0.65, 0.05, 0.36, 1) forwards;
+
+    @keyframes fadeTextIn {
+        from {
+            opacity: 0;
+            transform: translateY(40px);
+        }
+        to {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
 `;
 
 interface HeroHeaderProps {
