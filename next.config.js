b3abb5f66a7271bdcbe6512435086efe0a4974c6
Sandrico Provo <Sprovo@outlook.com>
2022-11-22 07:46:17 -0400

    [change] Updates next config file

--- next.config.js
+++ next.config.js
@@ -8,17 +8,17 @@
         // Enables the styled-components SWC transform
         styledComponents: true
     },
-    // webpack: (config, { isServer }) => {
-    //     // Fixes npm packages that depend on `fs` module on server
-    //     if (!isServer) {
-    //         config.resolve.fallback = {
-    //             ...config.resolve.fallback,
-    //             fs: false,
-    //         };
-    //     }
-    //     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    //     return config;
-    // },
+    webpack: (config, { isServer }) => {
+        // Fixes npm packages that depend on `fs` module on server
+        if (!isServer) {
+            config.resolve.fallback = {
+                ...config.resolve.fallback,
+                fs: false,
+            };
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return config;
+    },
   }
 
   module.exports = nextConfig
